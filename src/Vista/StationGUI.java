165e88259f9a1b2b1178ec6f6d5587d5771db971
WeidmannCamila <camiweid@gmail.com>
2021-07-12 20:31:22 -0300

    first commit

---
+++ src/Vista/StationGUI.java
@@ -0,0 +1,12 @@
+package Vista;
+
+import javax.swing.*;
+
+public class StationGUI {
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton eliminarEstacionButton;
+    private JButton añadirEstacionButton;
+    private JButton buscarEstacionButton;
+    private JButton modificarEstacionButton;
+}
---
+++ src/Vista/TransportGUI.java
@@ -0,0 +1,16 @@
+package Vista;
+
+import javax.swing.*;
+
+public class TransportGUI {
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton añadirTransporteButton;
+    private JButton button3;
+    private JButton modificarTransporteButton;
+    private JButton buscarButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}

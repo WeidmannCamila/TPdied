58b5ab2afac8192fef02879f5b77da14559c1d05
Geronimo Nicolas <ggallinger@frsf.utn.edu.ar>
2021-07-16 09:09:15 -0300

    Creacion de clases

---
+++ src/main/classes/Maintenance.java
@@ -0,0 +1,24 @@
+package main.classes;
+
+public class Maintenance {
+
+    private Integer idMaintenance;
+    private String description;
+
+    //getters and setters
+    public Integer getIdMaintenance() {
+        return idMaintenance;
+    }
+
+    public void setIdMaintenance(Integer idMaintenance) {
+        this.idMaintenance = idMaintenance;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/classes/Route.java
@@ -0,0 +1,92 @@
+package main.classes;
+
+import java.sql.Time;
+import java.util.List;
+
+public class Route {
+    private Integer idRoute;
+    private Station origin;
+    private Station destination;
+    private long distance;
+    private Time duration ;
+    private Integer maxPassagers;
+    private boolean status;
+    private Integer cost;
+    private List<Station> listStation;
+
+
+
+    //getters and setters
+
+    public Integer getIdRoute() {
+        return idRoute;
+    }
+
+    public void setIdRoute(Integer idRoute) {
+        this.idRoute = idRoute;
+    }
+
+    public Station getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Station origin) {
+        this.origin = origin;
+    }
+
+    public Station getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Station destination) {
+        this.destination = destination;
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
+    public void setDistance(long distance) {
+        this.distance = distance;
+    }
+
+    public Time getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Time duration) {
+        this.duration = duration;
+    }
+
+    public Integer getMaxPassagers() {
+        return maxPassagers;
+    }
+
+    public void setMaxPassagers(Integer maxPassagers) {
+        this.maxPassagers = maxPassagers;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public List<Station> getListStation() {
+        return listStation;
+    }
+
+    public void setListStation(List<Station> listStation) {
+        this.listStation = listStation;
+    }
+}

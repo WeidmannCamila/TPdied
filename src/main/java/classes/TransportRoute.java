7cb1239b23a57953a4381625159338c63c7ff1b8
Geronimo Nicolas <ggallinger@frsf.utn.edu.ar>
2021-07-19 15:31:39 -0300

    creacion de clases 2

---
+++ src/main/java/classes/TransportRoute.java
@@ -0,0 +1,44 @@
+package main.java.classes;
+
+public class TransportRoute {
+
+    private Integer idTransport;
+    private String name;
+    private String colour;
+    private boolean status;
+
+
+    //getters and setters
+
+    public Integer getIdTransport() {
+        return idTransport;
+    }
+
+    public void setIdTransport(Integer idTransport) {
+        this.idTransport = idTransport;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
